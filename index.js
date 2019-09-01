14ce85e41ff009c0ffe0f2f278e13a856041add6
natico <natamjadian.n@gmail.com>
2019-09-01 18:43:41 +0430

    secound commit

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 
-const courses =[
-    {id:1, name:'course1'},
-    {id:2, name:'course2'},
-    {id:3, name:'course3'},
+const courses = [{
+        id: 1,
+        name: 'course1'
+    },
+    {
+        id: 2,
+        name: 'course2'
+    },
+    {
+        id: 3,
+        name: 'course3'
+    },
 ];
-
 
 app.get('/', (req, res) => {
     res.send('hello world');
@@ -19,33 +27,62 @@ app.get('/api/courses', (req, res) => {
 });
 
 app.get('/api/courses/:id', (req, res) => {
-    
     let course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course) res.status(404).send('not found');
-    res.send(course);    
+    if (!course) return res.status(404).send('not found');
+    res.send(course);
 });
 
 
-app.post('/api/courses', (req, res) =>{
-    const course ={
-        id : courses.length +1,
-        name : req.body.name
+app.post('/api/courses', (req, res) => {
+    //Validate dta
+    const {
+        error
+    } = validateCourse(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
     };
 
-courses.push(course);
-res.send(course);
+    courses.push(course);
+    res.send(course);
 });
 
+app.put('/api/courses/:id', (req, res) => {
+    //Look up the course
+    //if not existing , return 404
+    let course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('not found');
 
+    //Validate dta
+    const {
+        error
+    } = validateCourse(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
+    //update course
+    course.name = req.body.name;
+    res.send(course);
+});
 
-// app.put();
-// app.delete();
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(course, schema);
+}
 
-
+app.delete('/api/courses/:id', (req, res) => {
+    //Look up the course
+    //if not existing , return 404
+    let course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('not found');
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+    res.send(course);
+});
 
 //PORT
-const port= process.env.PORT || 3000;
-app.listen(port, ()=>console.log(`Listening on port ${port} ...`));
-
-
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port} ...`));
